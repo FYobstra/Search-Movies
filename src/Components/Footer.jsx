c9ed6a7bf8dad38f2c85b13add4cb95e8fd36a85
FYobstra <facundolyobstraibizer@gmail.com>
2023-04-21 23:14:11 -0300

    bugFix

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -6,7 +6,7 @@ export default function Footer() {
   return (
     <div className="w-full border-t dark:border-t-gray-800 px-10 md:px-20 flex justify-between items-center py-5 mt-10">
       <p className="dark:text-gray-500 text-sm">
-        Echo por Facundo Yobstraibier
+        Hecho por Facundo Yobstraibier
       </p>
       <Link target="_blanck" to={"https://github.com/FYobstra"}>
         <FaGithub className="text-xl dark:text-gray-500 cursor-pointer" />
